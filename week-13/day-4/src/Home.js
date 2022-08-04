9b9ada0d082e2a47c57b925e85d6d9e86c09ab31
Joshua Cook <josh.cook@passportinc.com>
2022-08-04 07:53:47 -0400

    Added week 13 day 4

---
+++ week-13/day-4/src/Home.js
@@ -0,0 +1,56 @@
+const Home = (props) => {
+    const { title, price, beds, baths, pets, image, reviews, rating } = props;
+
+    return (
+        <div
+            class="overflow-hidden flex flex-col md:flex-row items-center bg-white md:p-8 p-6 gap-x-8 rounded-3xl mt-10 first:mt-0">
+            <div class="w-full md:w-auto">
+                <div class="aspect-square w-full md:w-48 flex-shrink-0 flex-grow-0 bg-zinc-200 rounded-2xl bg-center bg-cover"
+                    style={{
+                        backgroundImage: `url(${image})`
+                    }}></div>
+            </div>
+            <div class="w-full">
+                <div class="flex justify-between text-lg md:text-xl flex-col md:flex-row">
+                    <div class="font-semibold text-zinc-800 mt-4 md:mt-0">{title}</div>
+                    <div class="text-rose-500 font-semibold mt-1 md:mt-0">${price}/week</div>
+                </div>
+                <div
+                    class="flex flex-col md:flex-row items-start md:items-center text-sm md:text-base md:gap-5 text-zinc-500 mt-2">
+                    <div class="flex items-center gap-2">
+                        <i class="fa-solid fa-bed"></i>
+                        <span>{beds} beds</span>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <i class="fa-solid fa-shower"></i>
+                        <span>{baths} baths</span>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <i class="fa-solid fa-paw"></i>
+                        {pets == true && (
+                            <span>pets allowed</span>
+                        )}
+                        {pets == false && (
+                            <span>no pets allowed</span>
+                        )}
+                    </div>
+                </div>
+
+                <div
+                    class="border-t border-zinc-200 mt-3 pt-3 md:mt-6 md:pt-6 flex flex-col md:flex-row gap-y-2 items-center justify-between">
+                    <div class="flex gap-1 md:gap-2 text-zinc-600 text-sm md:text-base">
+                        <i class="fa-solid fa-star leading-6 text-yellow-400"></i> <span
+                            class="leading-7">{rating}&nbsp;({reviews}&nbsp;reviews)</span>
+                    </div>
+                    <div class="w-full md:w-auto">
+                        <button
+                            class="h-12 bg-rose-500 text-white px-8 font-semibold rounded-lg w-full">Book
+                            Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ week-13/day-4/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["src/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: "Poppins, sans-serif",
+            },
+        },
+    },
+    plugins: [],
+};
