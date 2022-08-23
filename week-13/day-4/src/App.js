332dea92ab7e243960b121ae2aeafd5c3994f3e6
Joshua Cook <josh.cook@passportinc.com>
2022-08-23 08:38:50 -0400

    Finished up week 14 day 4 lesson

--- week-13/day-4/src/App.js
+++ week-13/day-4/src/App.js
@@ -2,18 +2,30 @@ import React from 'react';
 import Home from './Home';
 
 function App() {
+    const [data, setData] = React.useState();
 
+    React.useEffect(() => {
+        fetch('https://data.mongodb-api.com/app/challenges-xlbkk/endpoint/featuredRentalListings')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data);
+                setData(data);
+            });
+    }, []);
 
     return (
         <div class="container max-w-5xl mx-auto p-2 md:p-8">
             <div class="bg-zinc-100 rounded-3xl p-5 md:p-10">
                 <h1 class="text-2xl md:text-3xl font-semibold text-zinc-800">Featured Homes</h1>
                 <div class="mt-4 md:mt-6">
-                    <Home title="Test title" price="3600" beds="4" baths="4" rating="4.9" reviews="18" petsAllowed={false} image="/assets/images/home-1.jpg" />
+                    {data && data.map(({ baths, beds, imageUrl, petsAllowed, pricePerWeek, rating, reviews, title }) => {
+                        return <Home title={title} price={pricePerWeek} beds={beds} baths={baths} rating={rating} reviews={reviews} petsAllowed={petsAllowed} image={imageUrl} />
+                    })}
                 </div>
                 <div class="text-center mt-8">
-                    <button class="border-2 border-zinc-200 h-12 rounded-lg px-8 font-semibold text-zinc-800">View
-                        all</button>
+                    <button class="border-2 border-zinc-200 h-12 rounded-lg px-8 font-semibold text-zinc-800">View all</button>
                 </div>
             </div>
         </div>
--- week-13/day-4/src/Home.js
+++ week-13/day-4/src/Home.js
@@ -2,33 +2,35 @@
 
 function Home(props) {
 
+    const { beds, baths, image, petsAllowed, price, rating, reviews, title } = props;
+
     return (
         <div
             class="overflow-hidden flex flex-col md:flex-row items-center bg-white md:p-8 p-6 gap-x-8 rounded-3xl mt-10 first:mt-0">
             <div class="w-full md:w-auto">
                 <div class="aspect-square w-full md:w-48 flex-shrink-0 flex-grow-0 bg-zinc-200 rounded-2xl bg-center bg-cover" style={{
-                    backgroundImage: 'url(' + props.image + ')'
+                    backgroundImage: 'url(' + image + ')'
                 }}></div>
             </div>
             <div class="w-full">
                 <div class="flex justify-between text-lg md:text-xl flex-col md:flex-row">
-                    <div class="font-semibold text-zinc-800 mt-4 md:mt-0">{props.title}</div>
-                    <div class="text-rose-500 font-semibold mt-1 md:mt-0">${props.price}/week</div>
+                    <div class="font-semibold text-zinc-800 mt-4 md:mt-0">{title}</div>
+                    <div class="text-rose-500 font-semibold mt-1 md:mt-0">${price}/week</div>
                 </div>
                 <div
                     class="flex flex-col md:flex-row items-start md:items-center text-sm md:text-base md:gap-5 text-zinc-500 mt-2">
                     <div class="flex items-center gap-2">
                         <i class="fa-solid fa-bed"></i>
-                        <span>{props.beds} beds</span>
+                        <span>{beds} beds</span>
                     </div>
                     <div class="flex items-center gap-2">
                         <i class="fa-solid fa-shower"></i>
-                        <span>{props.baths} baths</span>
+                        <span>{baths} baths</span>
                     </div>
                     <div class="flex items-center gap-2">
                         <i class="fa-solid fa-paw"></i>
-                        {props.petsAllowed == true && <span>pets allowed</span>}
-                        {props.petsAllowed == false && <span>no pets allowed</span>}
+                        {petsAllowed == true && <span>pets allowed</span>}
+                        {petsAllowed == false && <span>no pets allowed</span>}
                     </div>
                 </div>
 
@@ -36,7 +38,7 @@ function Home(props) {
                     class="border-t border-zinc-200 mt-3 pt-3 md:mt-6 md:pt-6 flex flex-col md:flex-row gap-y-2 items-center justify-between">
                     <div class="flex gap-1 md:gap-2 text-zinc-600 text-sm md:text-base">
                         <i class="fa-solid fa-star leading-6 text-yellow-400"></i> <span
-                            class="leading-7">{props.rating}&nbsp;({props.reviews}&nbsp;reviews)</span>
+                            class="leading-7">{rating}&nbsp;({reviews}&nbsp;reviews)</span>
                     </div>
                     <div class="w-full md:w-auto">
                         <button
